cca464de72ee5acc4fc5d12199d868ed4c292029
konishi0125 <yumemita@gmail.com>
2021-01-13 09:47:59 +0900

    add tqdm and comment

--- predict.py
+++ predict.py
@@ -1,9 +1,12 @@
+import glob
+from tqdm import tqdm 
+import numpy as np
+import pandas as pd
 import cv2
 from tensorflow import keras
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from keras_facenet import FaceNet
+
+
 
 name_list = pd.read_csv('./member_list.csv')
 name_list = name_list['name'].values.tolist()
@@ -53,7 +56,7 @@ def pred_face_for_img_path_list(img_path_list, model_path, cascade_path='./casca
     nose_cascade = cv2.CascadeClassifier(cascade_path+'/haarcascade_mcs_nose.xml')
     face_cascade = cv2.CascadeClassifier(cascade_path+'/haarcascade_frontalface_default.xml')
     model = keras.models.load_model(model_path)
-    for img_path in img_path_list:
+    for img_path in tqdm(img_path_list):
         img = cv2.imread(img_path)
         face = get_face(img, nose_cascade, face_cascade)
         if len(face) == 0:
@@ -69,6 +72,10 @@ def pred_face_for_img_path_list(img_path_list, model_path, cascade_path='./casca
     return result
 
 def main():
+    '''
+    メイン関数
+    '''
+    #画像のパス　形式はリスト
     img_path_list = ['./downloads/hina_kawata/1.1000_1000_102400.jpg']
     model_path = './model/facenet_hinata_model.h5'
     member_list =\
